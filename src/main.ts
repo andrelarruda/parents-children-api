f424ea44b8b0954243f72abec1e6f0e9d25b132f
Andre Arruda <andrebass27@gmail.com>
2024-10-13 23:07:09 -0300

    Setting up swagger.

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,20 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import "reflect-metadata";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Parents/children API REST')
+    .setDescription('API created just for practicing. Have fun :D')
+    .setVersion('1.0')
+    .addTag('parents/children')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger', app, document)
+
   await app.listen(3000);
 }
 bootstrap();
